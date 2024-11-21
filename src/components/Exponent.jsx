942bbc3d9e37e379d6d468fdb963edda55942c00
candela <candelap.r.p@gmail.com>
2024-11-21 20:44:50 +0100

    [WD-PT-RMT-SEP24]-candela rodriguez

---
+++ src/components/Exponent.jsx
@@ -0,0 +1,31 @@
+const Exponent = ({ num, exponent }) => {
+
+    const result = num ** exponent;
+
+    const numArray = [];
+
+    for (let i = 0; i < exponent; i++) {
+        numArray.push(num)
+    }
+
+    const expression = numArray.join(" * ")
+
+
+    return (
+        <div className="exponent-counter-container">
+            <p className="exponent-label">{num}^{exponent}</p>
+
+            <p className="exponent-result">{expression} = <span className="total">{result}</span></p>
+        </div>
+
+    );
+
+
+}
+
+
+export default Exponent;
+
+
+
+
